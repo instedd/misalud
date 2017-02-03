5de44d109fafd128d1f705c49335ccbc5d878f3c
Brian J. Cardiff <bcardiff@manas.com.ar>
2017-02-03 12:35:10 -0500

    update survey texts

--- app/models/message_processor.rb
+++ app/models/message_processor.rb
@@ -17,7 +17,7 @@ class MessageProcessor
         }
         r.no {
           update "pending_reason_not_seen", { "seen" => false }
-          send_sms "Why? Reply 1. You couldn’t get there 2. Your health status changed 3. Cost 4. Went but rejected"
+          send_sms "Why? Respond with one of the following numbers: 1.You couldn’t get there  2.Your health status changed  3.Cost  4.Went but refused service"
         }
         r.otherwise {
           send_sms "We didn't get that. Please reply 'yes' or 'no'."
@@ -27,7 +27,7 @@ class MessageProcessor
       respond_to do |r|
         r.digit(1,3) { |d|
           update "pending_satisfaction", { "clinic" => d }
-          send_sms "Are you satisfied? 1 ok, 5 best experience."
+          send_sms "Are you satisfied? 1. worst experience, 5. best experience."
         }
         r.otherwise {
           send_sms "We didn't get that. Please reply '1', '2' or '3'."
@@ -61,7 +61,7 @@ class MessageProcessor
       respond_to do |r|
         r.digit(1,3) { |d|
           update "pending_can_be_called", { "reason_not_seen" => NOT_SEEN_REASON[d] }
-          send_sms "can we call you back to talk about your experience? #{REPLY_YES_NO}"
+          send_sms CAN_BE_CALLED
         }
         r.digit(4,4) { |d|
           update "pending_clinic_not_seen", { "reason_not_seen" => NOT_SEEN_REASON[d] }
@@ -75,7 +75,7 @@ class MessageProcessor
       respond_to do |r|
         r.digit(1,3) { |d|
           update "pending_can_be_called", { "clinic" => d }
-          send_sms "can we call you back to talk about your experience? #{REPLY_YES_NO}"
+          send_sms CAN_BE_CALLED
         }
         r.otherwise {
           send_sms "We didn't get that. Please reply '1', '2' or '3'."
@@ -95,8 +95,9 @@ class MessageProcessor
   end
 
   REPLY_YES_NO = "Reply yes or no"
-  START_SURVEY = "Last time we recommend you some clinics. Where you seen by someone? #{REPLY_YES_NO}"
-  CLINIC_OPTIONS = "Which one you choose? Reply with 1. for NYC Free Clinic, 2. for Weill Cornell, 3. for Columbia"
+  START_SURVEY = "Last time we recommend some clinics to you. Were you seen by someone? #{REPLY_YES_NO}"
+  CLINIC_OPTIONS = "Which one did you choose? Reply with 1. for NYC Free Clinic, 2. for Weill Cornell, 3. for Columbia"
+  CAN_BE_CALLED = "Can we call you back later to talk about your experience? #{REPLY_YES_NO}"
   NOT_SEEN_REASON = { 1 => "could_not_get_there", 2 => "health_change", 3 => "cost", 4 => "rejected" }
 
   private
