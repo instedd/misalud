b3ba10e99c8d2968c556c44b5cef6c30b9beb51d
Brian J. Cardiff <bcardiff@manas.tech>
2017-02-23 15:42:37 -0300

    Add seen & rejected to clinics listings

--- app/listings/clinics_listing.rb
+++ app/listings/clinics_listing.rb
@@ -1,6 +1,12 @@
 class ClinicsListing < Listings::Base
 
-  model Clinic
+  model do
+    Clinic.select %{
+      clinics.*,
+      (SELECT count(*) FROM Contacts WHERE NOT(Contacts.survey_was_seen) AND Contacts.survey_chosen_clinic_id = clinics.id) as contacts_seen,
+      (SELECT count(*) FROM Contacts WHERE Contacts.survey_was_seen AND Contacts.survey_chosen_clinic_id = clinics.id) as contacts_rejected
+    }
+  end
 
   column :name
   column :address
@@ -8,6 +14,8 @@ class ClinicsListing < Listings::Base
   column :walk_in_schedule
   column :selected_times, title: "# Suggested"
   column :rated_times, title: "# Selected"
+  column :contacts_seen, title: "# Seen", sortable: false
+  column :contacts_rejected, title: "# Rejected", sortable: false
   column :avg_rating, title: "Rating" do |clinic, rating|
     '%.2f' % rating if rating
   end
