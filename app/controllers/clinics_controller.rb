c55dab30d68d2776836b474f0c6ec69f9a2390d2
Brian J. Cardiff <bcardiff@manas.tech>
2017-06-02 16:13:08 -0300

    #14 Add basic stats of clinics at the end of the listing

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,5 +1,9 @@
 class ClinicsController < ApplicationController
   def index
+    clinics = Clinic.without_deleted
+    @clinics_total = clinics.group(:borough).count
+    @clinics_women_total = clinics.where(women_care: true).group(:borough).count
+    @borough = Borough.all
   end
 
   def sync
